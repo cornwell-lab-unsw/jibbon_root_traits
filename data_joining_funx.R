1ab1a68d6b21f74a930533bd4ccfbc137017a426
wcornwell <wcornwell@gmail.com>
2016-09-14 15:56:23 +1000

    data checking script

--- data_joining_funx.R
+++ data_joining_funx.R
@@ -1,20 +1,18 @@
-library(plyr)
-
-air_porosity<-read.csv("data/air_porosity.csv")
-cluster_root_weight<-read.csv("data/cluster_root_weight_data.csv")
-dry_root_weight<-read.csv("data/dry_root_weight.csv")
-root_length_data<-read.csv("data/root_lenght_data.csv")
-
-# trying to get codes for data frames to matches
-dry_root_weight$species_fixed<-toupper(dry_root_weight$species_code)
-root_length_data$species_fixed<-sub("_altered","",root_length_data$File_Name)
+library(tidyverse)
 
 
-srl_df<-join(dry_root_weight,root_length_data,by="species_fixed",type="full")
-srl_df$srl<-srl_df$Kimura_length/1000/srl_df$weight_specific_root_length
+air_porosity<-read_csv("data/air_porosity.csv")
+cluster_root_weight<-read_csv("data/cluster_root_weight_data.csv")
+dry_root_weight_1<-read_csv("data/Weight_oven_dried.csv")
+dry_root_weight_2<-read_csv("data/dry_root_weight.csv")
+root_length_data<-read_csv("data/Root_Length_manual_V1.csv")
 
-hist(srl_df$srl)
 
-write.csv(srl_df,"outputs/srl_data_joined.csv")
+
+
+
+
+
+
 
 
